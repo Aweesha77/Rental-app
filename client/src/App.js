b3f444507640846d08f40e3f82bd468cec3c6c17
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-17 02:59:31 +0530

    adding files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+    </Routes>
+    </BrowserRouter>
     
     
     
