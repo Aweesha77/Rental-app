807f705ddea2dc29f0253daff250aaeb039612dd
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-17 06:55:53 +0530

    adding files

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>
+    <div className='register'>
+        <div className="register-content">
+            <form>
+                <input placeholder='firstName' name='firstName' required/>
+                <input placeholder='lastName' name='lastName' required/>
+                <input placeholder='email' name='email' type='email' required/>
+                <input placeholder='password' name='password' type='password' required/>
+                <input placeholder='confirmPassword' name='confirmPassword' type='password' required/>
+                <input type='file' name='profileImage' accept='image/' style={{display:"none"}} required/>
+            </form>
+        </div>
       
     </div>
   )
