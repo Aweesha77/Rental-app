d1a7aca2ab15dad8926dee2ea4eac9c732083ed6
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-17 13:24:00 +0530

    adding files

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -28,10 +28,14 @@ router.post("/register", upload.single('profileImage'),async (req, res) => {
         if (!(firstName && lastName && email && password)) {
             res.status(400).json({ message: "All input is required" });
         }
+
+
         const oldUser = await User.findOne({ email });
         if (oldUser) {
             return res.status(409).json({ message: "User already exists" });
         }
+
+
         encryptedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             firstName,
@@ -49,8 +53,32 @@ router.post("/register", upload.single('profileImage'),async (req, res) => {
         user.token = token;
         res.status(201).json(user);
 
+
         //the uploaded file is available req.files
         const profileImage=req.files.profileImage;
+        if(!profileImage){
+            return res.status(400).send("No file uploaded");
+        }
+
+        const profileImagePath=profileImage.path;
+
+        const salt=await bcrypt.genSalt(10);   //generate salt , A salt is a random string that is used to add additional data to the password before hashing it. This makes the hash more secure. The 10 is the number of rounds used to generate the salt. The higher the number, the more secure
+        const hashedPassword=await bcrypt.hash(password,salt);   //hash the password.get sault and password and combine it and generate hashpassword.
+
+        const newUser=new User({
+            firstName,
+            lastName,
+            email,
+            password:hashedPassword,
+            profileImagePath,
+        });
+
+
+        await newUser.save();      //save the user to the database
+        res.status(201).json({message:"User registered succefssfully"});
+
+
+        
     } catch (err) {
         console.log(err);
     }
